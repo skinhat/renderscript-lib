0513b78aff785dbd706231c878128e2cda3aa5af
skinhat <skinhat@hotmail.com>
2014-06-07 23:27:15 +1200

    Update linkedlist.rs

--- linkedlist.rs
+++ linkedlist.rs
@@ -1,28 +1,41 @@
 // Based upon http://www.thegeekstuff.com/2012/08/c-linked-list-example/ linked list example
 // Execute in Android with mScript.invoke_main();
 
-#pragma version(1)
-#pragma rs java_package_name(com.skinhat) 
+##pragma version(1)
+#pragma rs java_package_name(com.skinhat.segment)
 #pragma stateFragment(parent)
 
 #include "rs_debug.rsh"
+
+char *memory;
+static int memoryinc;
+
+static char  *malloc(int bytes)
+{
+   char*ret=memory+	memoryinc;
+	memoryinc+=bytes;
+   return ret;
+}
+
+
 struct test_struct
 {
-    int val;
+    short *val;
     void *next;
 };
 
 static struct test_struct *head = NULL;
 static struct test_struct *curr = NULL;
 
+static int silent=1;
 
-static struct test_struct* create_list( int val,struct test_struct *ptr)
+static struct test_struct* create_list( char *val,struct test_struct *ptr)
 {
-    rsDebug("\n creating list with headnode as [%d]\n",val);
-    //struct test_struct *ptr = (struct test_struct*)malloc(sizeof(struct test_struct));
+
     if(NULL == ptr)
     {
-        rsDebug("\n Node creation failed \n",1);
+	    if (!silent)
+    	    rsDebug("\n Node creation failed \n",1);
         return NULL;
     }
     ptr->val = val;
@@ -32,99 +45,102 @@ static struct test_struct* create_list( int val,struct test_struct *ptr)
     return ptr;
 }
 
-static struct test_struct* add_to_list(int val, bool add_to_end,struct test_struct *ptr)
+static struct test_struct* add_to_list(char *val, bool add_to_end,struct test_struct *ptr)
 {
     if(NULL == head) {
         
         return (create_list(val,ptr));
     }
 
-    if(add_to_end)
-        rsDebug("\n Adding node to end of list with value [%d]\n",val);
-    else
-        rsDebug("\n Adding node to beginning of list with value [%d]\n",val);
+    /*if (!silent){
 
-   // struct test_struct *ptr = (struct test_struct*)malloc(sizeof(struct test_struct));
-    if(NULL == ptr)
-    {
+	    if(add_to_end)
+	        rsDebug("\n Adding node to end of list with value [%d]\n");//,val[0],val[1],val[2]);
+	    else
+	        rsDebug("\n Adding node to beginning of list with value [%d]\n");//,val[0],val[1],val[2]);
+    }*/
+
+
+    if(NULL == ptr) {
         rsDebug("\n Node creation failed \n",1);
         return NULL;
     }
     ptr->val = val;
     ptr->next = NULL;
 
-    if(add_to_end)
-    {
+    if(add_to_end) {
         curr->next = ptr;
         curr = ptr;
-    }
-    else
-    {
+    }else {
         ptr->next = head;
         head = ptr;
     }
     return ptr;
 }
 
-static struct test_struct* search_in_list( int val, struct test_struct **prev)
+static struct test_struct* add_first(char *val,struct test_struct *ptr)
+{
+    add_to_list(val,false,ptr);
+}
+
+static struct test_struct* add_last(char *val,struct test_struct *ptr)
+{
+    add_to_list(val,true,ptr);
+}
+
+
+static struct test_struct* search_in_list(char *val, struct test_struct **prev)
 {
     struct test_struct *ptr = head;
     struct test_struct *tmp = NULL;
     bool found = false;
 
-    rsDebug("\n Searching the list for value [%d] \n",val);
+    //if (!silent)
+    //	rsDebug("\n Searching the list for value [%d] \n",val[0],val[1],val[2]);
 
-    while(ptr != NULL)
-    {
-        if(ptr->val == val)
-        {
+    while(ptr != NULL) {
+        if(ptr->val == val){
             found = true;
             break;
-        }
-        else
-        {
+        }  else   {
             tmp = ptr;
             ptr = ptr->next;
         }
     }
 
-    if(true == found)
-    {
+    if(true == found)  {
         if(prev)
             *prev = tmp;
         return ptr;
-    }
-    else
-    {
+    } else {
         return NULL;
     }
 }
 
-static int delete_from_list(int val)
+
+static int delete_from_list(char* val)
 {
     struct test_struct *prev = NULL;
     struct test_struct *del = NULL;
 
-    rsDebug("\n Deleting value [%d] from list\n",val);
+   // if (!silent)
+	//    rsDebug("\n Deleting value [%d] from list\n",val[0],val[1],val[2]);
 
     del = search_in_list(val,&prev);
-    if(del == NULL)
-    {
+    if(del == NULL) {
         return -1;
-    }
-    else
-    {
-        if(prev != NULL)
-            prev->next = del->next;
+    } else {
 
-        if(del == curr)
-        {
+        if(prev != NULL) {
+            prev->next = del->next;
+        } 
+        if(del == curr) {
             curr = prev;
-        }
-        else if(del == head)
-        {
+            if (del==head)
+                head=curr;
+        } else if(del == head){
             head = del->next;
-        }
+        } 
     }
 
    // free(del);
@@ -133,25 +149,74 @@ static int delete_from_list(int val)
     return 0;
 }
 
+static char* remove_first()
+{
+    char* ret=head->val;
+    delete_from_list(head->val);
+    return ret;
+}
+
+static char* remove_last()
+{
+    char* ret=curr->val;
+    delete_from_list(curr->val);
+    return ret;
+}
+
+static bool is_empty()
+{
+   
+    return head==NULL;
+}
+
+static int list_size()
+{
+  struct test_struct *ptr = head;
+    int cnt=0;
+    
+    while(ptr != NULL)
+    {
+	  
+        ptr = ptr->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+static void list_clear()
+{
+    memoryinc=0;
+    head=NULL;
+    curr=NULL;
+}
+
 static void print_list()
 {
     struct test_struct *ptr = head;
 
-    rsDebug("\n -------Printing list Start------- \n",1);
+    if (!silent)
+	    rsDebug("\n -------Printing list Start------- \n",1);
     while(ptr != NULL)
     {
-        rsDebug("\n [%d] \n",ptr->val);
+	    if (!silent) {
+	        short *p=ptr->val;
+    	    rsDebug("\n [%d] \n",p[0],p[1]);
+    	}
         ptr = ptr->next;
     }
-    rsDebug("\n -------Printing list End------- \n",1);
+    if (!silent)
+	    rsDebug("\n -------Printing list End------- \n",1);
 
     return;
 }
 
 void main()
 {
-
-
+    char memoryalloc[64000];
+    memory=memoryalloc;
+    memoryinc=0;
+    
+    
     int i = 0, ret = 0;
     struct test_struct *ptr = NULL;
 
@@ -161,44 +226,24 @@ void main()
     print_list();
 
     for(i = 5; i<10; i++) {
-        add_to_list(i,true,&test_structs[structinc++]);
+        short *p=malloc(2*sizeof(short));
+        p[0]=i;
+        p[1]=i*2;
+        struct test_struct *element= malloc(sizeof(struct test_struct));
+     
+        add_first(p,element);
+        
     }
 
-    print_list();
+    rsDebug("size",list_size());
 
-    for(i = 4; i>0; i--) {
-      
-        add_to_list(i,false,&test_structs[structinc++]);
+    while (!is_empty()) {
+        short *p=remove_first();
+        rsDebug("p[0] p[1]",p[0],p[1]);
     }
+    rsDebug("size after ",list_size());
 
-    print_list(head);
-
-    for(i = 1; i<10; i += 4)
-    {
-        ptr = search_in_list(i, NULL);
-        if(NULL == ptr)
-        {
-            rsDebug("\n Search [val = %d] failed, no such element found\n",i);
-        }
-        else
-        {
-            rsDebug("\n Search passed [val = %d]\n",ptr->val);
-        }
-
-        print_list();
-
-        ret = delete_from_list(i);
-        if(ret != 0)
-        {
-            rsDebug("\n delete [val = %d] failed, no such element found\n",i);
-        }
-        else
-        {
-            rsDebug("\n delete [val = %d]  passed \n",i);
-        }
-
-        print_list();
-    }
+    
 
     //return 0;
 }
